354387f1cf89f131e06c70e06e1230f72199c4b9
Oleg Budaev <t0r1n88@mail.ru>
2024-01-12 15:33:45 +0800

    реализовал запись основной части колонок в фис фрдо

---
+++ create_documents.py
@@ -0,0 +1,11 @@
+"""
+Скрипт для создания сопроводительной документации
+Основной скрипт
+"""
+from create_fis_frdo import
+
+import pandas as pd
+import openpyxl
+from tkinter import messagebox
+import os
+from datetime import datetime
