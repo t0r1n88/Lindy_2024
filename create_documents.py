d6150e374ef6edc1d554f1645033d87a82e91cfa
Oleg Budaev <t0r1n88@mail.ru>
2024-01-19 12:42:53 +0800

    Добавил проверку на наличие папки фис фрдо

--- create_documents.py
+++ create_documents.py
@@ -107,8 +107,13 @@ def create_docs(data_file:str,folder_template:str,result_folder:str):
                 lst_date_columns_data.append(idx)
         data_df = convert_string_date(data_df,lst_date_columns_data)
 
-        # Создаем файл ФИС-ФРДО
-        create_fis_frdo(data_df,descr_df,folder_template,result_folder,type_program,data_file)
+        # Создаем файл ФИС-ФРДО Если нет папки или файлов то ничего не создаем
+        if os.path.exists(f'{folder_template}/ФИС-ФРДО/Шаблон ФИС-ФРДО ДПО.xlsx') and os.path.exists(f'{folder_template}/ФИС-ФРДО/Шаблон ФИС-ФРДО ПО.xlsx'):
+            create_fis_frdo(data_df,descr_df,folder_template,result_folder,type_program,data_file)
+        else:
+            messagebox.showwarning('Создание документов ДПО,ПО',f'ПРЕДУПРЕЖДЕНИЕ !!!\n В папке {folder_template} не найдена папка ФИС-ФРДО или файлы шаблонов в этой папке.\n'
+                                   'В папке ФИС-ФРДО должно быть 2 файла, эти файлы должны иметь название Шаблон ФИС-ФРДО ПО и Шаблон ФИС-ФРДО ДПО.\n'
+                                                                'Отсутствие этой папки НЕ ПОВЛИЯЕТ на создание остальных документов.')
 
         # создаем словари с данными для колонок описания программы
 
@@ -130,17 +135,6 @@ def create_docs(data_file:str,folder_template:str,result_folder:str):
 
 
 
-
-
-
-
-
-
-
-
-
-        # data_df.to_excel('data/Результат/dasd.xlsx',index=False,header=True)
-        # descr_df.to_excel('data/Результат/Исходник Описание.xlsx',index=False,header=True)
 
 
 
